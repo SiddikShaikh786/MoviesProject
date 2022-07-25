00ddd9c45b1b5e1a115269d5171c5808e8ec96aa
Siddik Shaikh <siddik.shaikh@collabera.com>
2022-07-25 21:05:00 +0530

    Changes made

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute} from "@angular/router";
 import { IMAGES_SIZES } from '../../constants/images-sizes';
 import { Item } from './item';
 @Component({
@@ -8,10 +9,26 @@ import { Item } from './item';
 })
 export class ItemComponent implements OnInit {
   @Input() itemData: Item | null = null;
-
+  // result!:string;
   imagesSizes = IMAGES_SIZES;
 
-  constructor() {}
+  constructor(private route:ActivatedRoute) {
 
-  ngOnInit(): void {}
+  }
+
+  ngOnInit(): void {
+// this.route.url.subscribe((value)=>{
+//   let path=value[value.length - 1].path
+//   if(path==='tvshows'){
+//     this.result='tv'
+//   }
+//   else if(path === 'movie'){
+
+//   }
+//   else{
+//     this.result='movie'
+//   }
+// });
+//   }
+}
 }
